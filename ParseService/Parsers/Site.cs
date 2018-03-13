b72d29f34a564c75b9c3bd8c2240bd9d4d20f2b7
Кривенко Александр <as.krivenko@stalcom.com>
2018-03-13 16:13:34 +0300

    Комментарии для Site.cs.

--- ParseService/Parsers/Site.cs
+++ ParseService/Parsers/Site.cs
@@ -1,19 +1,40 @@
 ﻿namespace ParseService.Parsers
 {
+	/// <summary>
+	/// Представляет Site
+	/// </summary>
 	public class Site
 	{
+		/// <summary>
+		/// Возвращает или устанавливает заголовок сайта.
+		/// </summary>
+		/// <value>
+		///Заголовок.
+		/// </value>
 		public string Title
 		{
 			get;
 			set;
 		}
 
+		/// <summary>
+		/// Возвращает или устанавливает Uri сайта.
+		/// </summary>
+		/// <value>
+		/// URI.
+		/// </value>
 		public string Uri
 		{
 			get;
 			set;
 		}
 
+		/// <summary>
+		/// Возвращает или устанавливает описание.
+		/// </summary>
+		/// <value>
+		/// Описание.
+		/// </value>
 		public string Description
 		{
 			get;
